87c11e7ad7ca00b2a0a9846696cf7f8d16bd2dd5
Klavdia <kgold94@yahoo.com>
2025-02-03 09:37:47 -0800

    add all files

---
+++ pages/home.page.ts
@@ -0,0 +1,20 @@
+import { Page, Locator } from '@playwright/test'
+
+export class HomePage {
+    private readonly page: Page;
+    private readonly searchTab: Locator
+
+    constructor(page: Page) {
+        this.page = page;
+        this.searchTab = page.getByRole('link', {name: 'Search', exact: true});
+    }
+
+    async clickSearchTab() {
+        await this.searchTab.click();
+        await this.page.waitForLoadState('networkidle');
+
+        // return new SearchPage(this.page);
+    }
+
+
+}
